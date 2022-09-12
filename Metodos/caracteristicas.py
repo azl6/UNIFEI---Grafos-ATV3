18ae7c0bc952d16b469116a3eabe7d2e8b5289b4
Alex <alex.rodrigues23997@gmail.com>
2022-09-11 23:33:07 -0300

    Adicionando comentários

--- Metodos/caracteristicas.py
+++ Metodos/caracteristicas.py
@@ -23,25 +23,33 @@ def verificaAdjacencia(matriz, vi, vj):
 #Descrição: Retorna o tipo do grafo representado por uma dada matriz de adjacências.
 def tipoGrafo(matriz):
 
+    #Condições para identificação dos grafos
     diagonalEhZerada = True
     contemParalelas = False
     ehSimetrica = True
 
+    #Encontrando a quantidade de vértices para percorrer a matriz de adjacência corretamente
     qtdVertices = np.shape(matriz)[0]
 
     for vi in range(0, qtdVertices):
         for vj in range(vi + 1, qtdVertices):
 
+            #Verifica se é uma posição da diagonal
             if vi == vj:
+
+                #Verifica se uma posição da diagonal é zerada
                 if matriz[vi][vj] != 0:
                     diagonalEhZerada = False
 
+            #Verifica se a matriz contém arestas paralelas
             if matriz[vi][vj] == 2:
                 contemParalelas = True
 
+            #Verifica se a matriz é simétrica ou assimétrica
             if matriz[vi][vj] != matriz[vj][vi]:
                 ehSimetrica = False
 
+    #Condicionais para identificar o grafo
     if diagonalEhZerada and not contemParalelas and ehSimetrica: #SIMPLES
         return 0
     if diagonalEhZerada and not contemParalelas and not ehSimetrica: #DIGRAFO
@@ -54,15 +62,21 @@ def tipoGrafo(matriz):
 #Descrição: Retorna o valor da densidade do grafo.
 def calcDensidade(self, matriz):
 
+    #Verificando o tipo do grafo
     tipoGrafo = self.tipoGrafo(matriz)
+
+    #Contando vértices
     qtdVertices = np.shape(matriz)[0]
 
     arestas=0
+
+    #Contando arestas para a fórmula
     for vi in range(0, qtdVertices):
         for vj in range(vi + 1, qtdVertices):
             if matriz[vi][vj] == 1:
                 arestas+=1
 
+    #Executando a fórmula baseado no tipo do grafo
     if tipoGrafo == 0:
         return (2 * arestas) / (qtdVertices * (qtdVertices - 1))
 
@@ -72,6 +86,8 @@ def calcDensidade(self, matriz):
 def insereAresta(self, matriz, vi, vj):
     print("Inserindo aresta...")
     tipoGrafo = self.tipoGrafo(matriz)
+
+    #Condicional baseada no tipo do grafo, para manter a simetria em grafos não-dirigidos
     if tipoGrafo == 1:
         matriz[vi][vj] = 1
     else:
@@ -84,13 +100,18 @@ def insereAresta(self, matriz, vi, vj):
 def insereVertice(matriz, vi):
     print("Inserindo vértice...")
     shape = matriz.shape
+
+    #Criando uma nova matriz de zeros, com as dimensões da anterior + 1
     novaMatriz = numpy.zeros((shape[0] + 1, shape[1] + 1))
 
     qtdVertices = np.shape(matriz)[0]
+
+    #Transferindo os valores da matriz antiga para a nova
     for vi in range(0, qtdVertices):
         for vj in range(0, qtdVertices):
             novaMatriz[vi][vj] = matriz[vi][vj] #a nova matriz recebe os valores da matriz antiga
 
+    #Retornando a nova matriz
     return novaMatriz
 
 #Remove aresta: Remove uma aresta do grafo considerando o par de vértices vi e vj.
@@ -98,6 +119,7 @@ def removeAresta(self, matriz, vi, vj):
     tipoGrafo = self.tipoGrafo(matriz)
     print("Removendo aresta...")
 
+    #Condicional baseada no tipo do grafo, para manter a simetria em grafos não-dirigidos
     if tipoGrafo == 1:
         matriz[vi][vj] = 0
     else:
